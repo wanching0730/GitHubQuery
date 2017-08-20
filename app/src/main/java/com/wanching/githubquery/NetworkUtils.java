bf302901fd02f448b4333f657bd803cbbc8331d7
Wan Ching <wanching_97@hotmail.com>
2017-08-20 23:33:32 +0800

    done polish

---
+++ app/src/main/java/com/wanching/githubquery/NetworkUtils.java
@@ -0,0 +1,71 @@
+package com.wanching.githubquery;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by WanChing on 20/8/2017.
+ */
+
+public class NetworkUtils {
+
+    final static String GITHUB_BASE_URL = "https://api.github.com/search/repositories";
+    final static String PARAM_QUERY = "q";
+    final static String PARAM_SORT = "sort";
+    final static String sortBy = "stars";
+
+    /**
+     * Builds the URL used to query Github.
+     *
+     * @param githubSearchQuery The keyword that will be queried for.
+     * @return The URL to use to query the weather server.
+     */
+    public static URL builURL(String githubSearchQuery){
+        Uri builtUri = Uri.parse(GITHUB_BASE_URL).buildUpon()
+                .appendQueryParameter(PARAM_QUERY, githubSearchQuery)
+                .appendQueryParameter(PARAM_SORT, sortBy)
+                .build();
+
+        URL url = null;
+        try{
+            url = new URL(builtUri.toString());
+        }catch (MalformedURLException ex){
+            ex.printStackTrace();
+        }
+
+        return url;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException{
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try{
+            InputStream is = connection.getInputStream();
+
+            Scanner scanner = new Scanner(is);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if(hasInput){
+                return scanner.next();
+            }else {
+                return null;
+            }
+        }finally {
+            connection.disconnect();
+        }
+    }
+}
